e9ddd2a1088091841c83ac73d4574ace229479f2
marconesb <marconebritt@gmail.com>
2024-04-13 23:49:34 -0300

    ...........

--- src/Components/Projetos.jsx
+++ src/Components/Projetos.jsx
@@ -1,107 +1,82 @@
 import React from 'react';
-import { Flipper, Flipped } from 'react-flip-toolkit';
-import { motion } from 'framer-motion';
-import karinaCevalles from '../assets/img/KarinaCevalles.png';
-import lacosdeamor from '../assets/img/lacosdeamor-2.png';
-import TrabalhadoresDaUltimaHora from '../assets/img/trabalhadoresdaultimahora.png';
-import solardejesus from '../assets/img/solardejeus-2.png';
-import Avatar from '../assets/img/avatar2.png';
-import downloadPDF from '../assets/img/Doc1.pdf';
-import '../Components/Projetos/Projetos.css'; // Adicionei a importação do arquivo CSS
 import { useInView } from 'react-intersection-observer';
+import { motion } from 'framer-motion';
+import KarinaCevalles from '../assets/img/KarinaCevalles.png';
+import Lacosdeamor from '../assets/img/lacosdeamor-2.png';
+import SolarDeJesus from '../assets/img/solardejeus-2.png';
+import TrabalhadoresDaUltimaHora from '../assets/img/trabalhadoresdaultimahora.png';
+import Avatar2 from '../assets/img/avatar2.png';
+import Curriculo from '../assets/img/Currículo  7º Período - Marcone Silva de Brito.pdf';
 
-const ProjectCard = ({ imageUrl, title, description, link, index }) => {
+const projects = [
+  {
+    title: 'Projeto 1',
+    description: 'Site desenvolvido para a Advogada Karina Cevalles',
+    imageUrl: KarinaCevalles,
+    link: 'https://site-karina-cevalles.vercel.app',
+  },
+  {
+    title: 'Projeto 2',
+    description: 'Site desenvolvido para a Associação Laços de Amor',
+    imageUrl: Lacosdeamor,
+    link: 'https://site-lacosdeamor.vercel.app',
+  },
+  {
+    title: 'Projeto 3',
+    description: 'Site criado para a Instituição de Caridade Solar de Jesus.',
+    imageUrl: SolarDeJesus,
+    link: 'https://www.solardejesus.com.br',
+  },
+  {
+    title: 'Projeto 4',
+    description: 'Site da Instituição de Caridade Trabalhadores da Última Hora.',
+    imageUrl: TrabalhadoresDaUltimaHora,
+    link: 'https://marconesdb.github.io/SiteTrabalhadoresDaUltimaHora',
+  },
+];
+
+const ProjectCard = ({ title, description, imageUrl, link, delay }) => {
   const { ref, inView } = useInView({
     threshold: 0.5,
     triggerOnce: true,
   });
 
-  const handleCardClick = (event) => {
-    if (event.target.tagName !== 'A') {
-      window.open(link, '_blank');
-    }
-  };
-
   return (
-    <Flipper flipKey={link}>
-      <Flipped flipId={`project-${link}`}>
-        <motion.div
-          ref={ref}
-          className="relative overflow-hidden rounded-lg border-2 border-gray-300"
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
-          transition={{ duration: 1, delay: index * 0.9 }}
-          onClick={handleCardClick}
-        >
-          <div className="m-2">
-            <img src={imageUrl} alt={title} className="object-cover w-full h-64" />
-            <div className="absolute inset-0 bg-black opacity-0 hover:opacity-50 transition-opacity duration-300 flex items-center justify-center">
-              <h2 className="text-white text-lg font-semibold">{title}</h2>
-            </div>
-            <div className="absolute inset-x-0 bottom-0 px-4 py-2 bg-white bg-opacity-75">
-              <p className="text-gray-800 text-sm">{description}</p>
-            </div>
-          </div>
-        </motion.div>
-      </Flipped>
-    </Flipper>
+    <motion.div
+      ref={ref}
+      className="shadow-xl rounded-lg overflow-hidden border-2 border-gray-300"
+      initial={{ opacity: 0, x: -100 }}
+      animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : -100 }}
+      transition={{ duration: 0.5, delay }}
+    >
+      <img className="w-full h-64 object-cover object-center" src={imageUrl} alt={title} />
+      <div className="p-4">
+        <h2 className="text-amber-600 font-semibold text-xl mb-2">{title}</h2>
+        <p className="text-gray-600 text-base">{description}</p>
+        <a href={link} className="block text-center mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Ver mais
+        </a>
+      </div>
+    </motion.div>
   );
 };
 
-const ProjectList = ({ projects }) => {
-  return (
-    <div id="" className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center mt-4">
+const App = () => (
+  <div className=" mx-auto px-4 py-8 bg-gradient-to-r from-black via-indigo-900 to-black bg-fixed">
+    <h1 className="text-3xl font-bold text-center text-amber-400 mt-28">Projetos</h1>
+    <div className="flex justify-start mb-8">
+      <a href={Curriculo} download="Curriculo.pdf" target="_blank" className=" block bg-blue-500 hover:bg-blue-700 w-32 font-bold py-2 px-4 rounded text-amber-400">Download CV</a>
+    </div>
+    <div className="flex justify-center mb-8">
+      <img className="w-24 h-24" src={Avatar2} alt="Imagem de exemplo" />
+      <p className='text-yellow-600 text-justify flex justify-center text-orange-30 text-2xl mb-8 mt-8 mr-8'><em>"Explore meus projetos e entre em contato para colaborações."</em></p>
+    </div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-32">
       {projects.map((project, index) => (
-        <ProjectCard key={index} {...project} index={index} />
+        <ProjectCard key={index} {...project} delay={index * 0.5} />
       ))}
     </div>
-  );
-};
+  </div>
+);
 
-export default function Projetos() {
-  const projects = [
-    {
-      imageUrl: karinaCevalles,
-      title: '',
-      description: 'Site desenvolvido para a Advogada karina Cevalles.',
-      link: 'https://github.com/marconesdb/site-karinaCevalles',
-    },
-    {
-      imageUrl: lacosdeamor,
-      title: '',
-      description: 'Site da Instituição Filantrópica Laços de Amor',
-      link: 'https://github.com/marconesdb/site-lacosdeamor',
-    },
-    {
-      imageUrl: TrabalhadoresDaUltimaHora,
-      title: '',
-      description: 'Site da Instituição de Caridade Trabalhadores da Última Hora.',
-      link: 'https://github.com/marconesdb/SiteTrabalhadoresDaUltimaHora',
-    },
-    {
-      imageUrl: solardejesus,
-      title: '',
-      description: 'Site da Instituição Religiosa Solar de Jesus',
-      link: 'https://github.com/marconesdb/solarDeJesus-React',
-    },
-  ];
-
-  return (
-    <div id="projetos" className="p-24 bg-gradient-to-r from-black via-indigo-900 to-black bg-fixed flex flex-col justify-center items-center">
-      <div className="w-full max-w-screen-lg px-4">
-        <h1 className="text-3xl font-bold mt-4 mb-8 text-center text-amber-400">Projetos</h1>
-        <a href={downloadPDF} download="Example-PDF-document" target="_blank" className="block bg-blue-500 hover:bg-blue-700 w-32 font-bold py-2 px-4 rounded text-amber-400">
-          Download CV
-        </a>
-
-        <div className="flex justify-center">
-          <img className="w-36 h-36 rounded-xl" src={Avatar} alt="" />
-        </div>
-        <p className='text-yellow-600 text-justify flex justify-center text-orange-30 text-2xl mb-8 mt-8'><em>"Explore meus projetos e entre em contato para colaborações."</em></p>
-        <div className="mx-auto mb-4">
-          <ProjectList projects={projects} />
-        </div>
-      </div>
-    </div>
-  );
-}
+export default App;
